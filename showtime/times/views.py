08f1f5cdeac4fe974d673b7d93f320833ef36bb6
temuulen <143776127+huslen23@users.noreply.github.com>
2024-01-29 16:22:11 +0800

    3

--- showtime/times/views.py
+++ showtime/times/views.py
@@ -1,8 +1,15 @@
 from django.http import JsonResponse
+import json
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def showtime(request):
-    time= datetime.now()
-    return JsonResponse({"time":time})
+    if request.method=="POST":
+        data=json.loads(request.body)
+        action=data.get("action")
+        if action=="gettime":
+            time= datetime.now()
+            return JsonResponse({"time":time})
 
 
